5307130ac6f6cfd171567690797fec6d27626e40
Mykhailo Polishchuk <mpolishchuk86@gmail.com>
2023-02-04 11:40:19 +0200

    Created EventSerializer for the Event model

--- event_management/event/serializers.py
+++ event_management/event/serializers.py
@@ -12,3 +12,22 @@ class EventTypeSerializer(serializers.ModelSerializer):
 
         model = EventType
         fields = ['name']
+
+
+class EventSerializer(serializers.ModelSerializer):
+    """Serializer for Event Model."""
+
+    event_type = serializers.CharField(max_length=256, required=True)
+
+    class Meta:
+        """Class with a model and model fields for serialization."""
+
+        model = Event
+        fields = '__all__'
+        read_only_fields = ('id', 'user', 'created_at')
+
+    def validate_event_type(self, name):
+        """Get existing event type or create new."""
+
+        event_type, _ = EventType.objects.get_or_create(name=name)
+        return event_type
