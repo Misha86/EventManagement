c4fabd9bbf8fed3df56fc6f97f42302f572f774b
Mykhailo Polishchuk <mpolishchuk86@gmail.com>
2023-02-04 08:29:48 +0200

    Created Event model

--- event_management/event/models.py
+++ event_management/event/models.py
@@ -1,3 +1,42 @@
-from django.db import models
+"""Module for all project models."""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext as _
+import uuid
+
+User = get_user_model()
+
+
+class Event(models.Model):
+    """This class represents a basic Event (for an event system).
+
+    Attributes:
+        id (uuid4): Primary key type uuid4
+        user (int): User id who creates event
+        event_type (int): Represents event type id
+        info (json): Some information about concrete event
+        timestamp (datetime): Event time and date
+        created_at (datetime): Time of creation of the event
+    """
+
+    help_texts = {'required': _('This field is required')}
+
+    id = models.UUIDField('UUID', primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, related_name='events', on_delete=models.CASCADE, verbose_name=_('User'))
+    event_type = models.ForeignKey(EventType, related_name='events', on_delete=models.CASCADE,
+                                   verbose_name=_('Event Type'))
+    info = models.JSONField(_('Event info'), help_text=help_texts['required'])
+    timestamp = models.DateTimeField(_('Event datetime'), help_text=help_texts['required'])
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('Created at'))
+
+    class Meta:
+        """This meta class stores verbose names and ordering data."""
+
+        ordering = ['id']
+        verbose_name_plural = _('Events')
+
+    def __str__(self) -> str:
+        """str: Returns class name and instance id."""
+        return f"{self.__class__.__name__} #{self.id}"
+
